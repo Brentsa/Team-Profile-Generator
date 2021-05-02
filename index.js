af30e95c4544f5c0f6490f99841b71d9e9b4e1e0
Solidshots <seanbrent5@gmail.com>
2021-05-02 09:12:53 -0400

    Version 1.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Application = require('./lib/Application');
 
+//Instantiate a new application and then run it as program start
+//=================================================================================
 const application = new Application;
-
 application.promptManager();
     
